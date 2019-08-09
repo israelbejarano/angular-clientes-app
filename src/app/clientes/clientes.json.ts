f72989496496fa819b2c1cd380ae47f622be6577
israel <israel.bejarano.a@gmail.com>
2019-08-09 23:42:37 +0200

    changed data

--- src/app/clientes/clientes.json.ts
+++ src/app/clientes/clientes.json.ts
@@ -7,9 +7,9 @@ export const CLIENTES: Cliente[] = [
     {id: 4, nombre: 'Jane', apellido: 'Doe', email: 'jane.doe@gmail.com', createAt: '2019-08-09'},
     {id: 5, nombre: 'Rasmus', apellido: 'Lerdorf', email: 'rasmus.lerdorf@gmail.com', createAt: '2019-08-09'},
     {id: 6, nombre: 'Erich', apellido: 'Gamma', email: 'erich.gamma@gmail.com', createAt: '2019-08-09'},
-    {id: 7, nombre: 'Richard', apellido: 'Helm', email: 'richard.helm@gmail.com', createAt: '2019-08-09'},
-    {id: 8, nombre: 'Ralph', apellido: 'Johnson', email: 'ralph.johnson@gmail.com', createAt: '2019-08-09'},
-    {id: 9, nombre: 'John', apellido: 'Vlissides', email: 'john.vlissides@gmail.com', createAt: '2019-08-09'},
-    {id: 10, nombre: 'James', apellido: 'Gosling', email: 'james.gosling@gmail.com', createAt: '2019-08-09'},
-    {id: 11, nombre: 'Bruce', apellido: 'Lee', email: 'bruce.lee@gmail.com', createAt: '2019-08-09'},
+    {id: 7, nombre: 'Richard', apellido: 'Helm', email: 'richard.helm@gmail.com', createAt: '2019-08-09'}
+    // {id: 8, nombre: 'Ralph', apellido: 'Johnson', email: 'ralph.johnson@gmail.com', createAt: '2019-08-09'},
+    // {id: 9, nombre: 'John', apellido: 'Vlissides', email: 'john.vlissides@gmail.com', createAt: '2019-08-09'},
+    // {id: 10, nombre: 'James', apellido: 'Gosling', email: 'james.gosling@gmail.com', createAt: '2019-08-09'},
+    // {id: 11, nombre: 'Bruce', apellido: 'Lee', email: 'bruce.lee@gmail.com', createAt: '2019-08-09'},
   ];
