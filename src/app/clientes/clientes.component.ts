6b162acfb5fc31c802e9d3985ae42f7d26ce9daa
israel <israel.bejarano.a@gmail.com>
2019-08-09 22:38:56 +0200

    mostrar la lista de clientes

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente';
 
 @Component({
   selector: 'app-clientes',
@@ -6,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clientes.component.css']
 })
 export class ClientesComponent implements OnInit {
+
+  clientes: Cliente[] = [
+    {id: 1, nombre: 'Israel', apellido: 'Bejarano', email: 'prueba@prueba.com', createAt: '2019-08-09'},
+    {id: 2, nombre: 'John', apellido: 'Doe', email: 'john.doe@gmail.com', createAt: '2019-08-09'},
+    {id: 3, nombre: 'Linus', apellido: 'Torvalds', email: 'linus.torvalds@gmail.com', createAt: '2019-08-09'},
+    {id: 4, nombre: 'Jane', apellido: 'Doe', email: 'jane.doe@gmail.com', createAt: '2019-08-09'},
+    {id: 5, nombre: 'Rasmus', apellido: 'Lerdorf', email: 'rasmus.lerdorf@gmail.com', createAt: '2019-08-09'},
+    {id: 6, nombre: 'Erich', apellido: 'Gamma', email: 'erich.gamma@gmail.com', createAt: '2019-08-09'},
+    {id: 7, nombre: 'Richard', apellido: 'Helm', email: 'richard.helm@gmail.com', createAt: '2019-08-09'},
+    {id: 8, nombre: 'Ralph', apellido: 'Johnson', email: 'ralph.johnson@gmail.com', createAt: '2019-08-09'},
+    {id: 9, nombre: 'John', apellido: 'Vlissides', email: 'john.vlissides@gmail.com', createAt: '2019-08-09'},
+    {id: 10, nombre: 'James', apellido: 'Gosling', email: 'james.gosling@gmail.com', createAt: '2019-08-09'},
+    {id: 11, nombre: 'Bruce', apellido: 'Lee', email: 'bruce.lee@gmail.com', createAt: '2019-08-09'},
+  ];
 
   constructor() { }
 
