0081b8321dadf8ce3eab9a60b060e6af4750e052
israel <israel.bejarano.a@gmail.com>
2019-08-09 22:50:08 +0200

    separar los datos en fichero aparte

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { CLIENTES } from './clientes.json';
 
 @Component({
   selector: 'app-clientes',
@@ -8,23 +9,12 @@ import { Cliente } from './cliente';
 })
 export class ClientesComponent implements OnInit {
 
-  clientes: Cliente[] = [
-    {id: 1, nombre: 'Israel', apellido: 'Bejarano', email: 'prueba@prueba.com', createAt: '2019-08-09'},
-    {id: 2, nombre: 'John', apellido: 'Doe', email: 'john.doe@gmail.com', createAt: '2019-08-09'},
-    {id: 3, nombre: 'Linus', apellido: 'Torvalds', email: 'linus.torvalds@gmail.com', createAt: '2019-08-09'},
-    {id: 4, nombre: 'Jane', apellido: 'Doe', email: 'jane.doe@gmail.com', createAt: '2019-08-09'},
-    {id: 5, nombre: 'Rasmus', apellido: 'Lerdorf', email: 'rasmus.lerdorf@gmail.com', createAt: '2019-08-09'},
-    {id: 6, nombre: 'Erich', apellido: 'Gamma', email: 'erich.gamma@gmail.com', createAt: '2019-08-09'},
-    {id: 7, nombre: 'Richard', apellido: 'Helm', email: 'richard.helm@gmail.com', createAt: '2019-08-09'},
-    {id: 8, nombre: 'Ralph', apellido: 'Johnson', email: 'ralph.johnson@gmail.com', createAt: '2019-08-09'},
-    {id: 9, nombre: 'John', apellido: 'Vlissides', email: 'john.vlissides@gmail.com', createAt: '2019-08-09'},
-    {id: 10, nombre: 'James', apellido: 'Gosling', email: 'james.gosling@gmail.com', createAt: '2019-08-09'},
-    {id: 11, nombre: 'Bruce', apellido: 'Lee', email: 'bruce.lee@gmail.com', createAt: '2019-08-09'},
-  ];
+  clientes: Cliente[];
 
   constructor() { }
 
   ngOnInit() {
+    this.clientes = CLIENTES;
   }
 
 }
